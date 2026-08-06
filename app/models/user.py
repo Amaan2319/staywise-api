604d7bdd88aa85afa8a61d23ffd3a13d327e6efc
Amaan2319 <109417887+Amaan2319@users.noreply.github.com>
2026-08-06 08:39:24 +0530

    alembic for database migrations

---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+from enum import Enum
+from sqlalchemy import Column,Integer,String,Enum as SqlEnum
+from app.database.database import Base
+
+class UserRole(Enum):
+  OWNER="OWNER"
+  TENANT="TENANT"
+
+class User(Base):
+  __tablename__ = 'users'
+  
+  id=Column(Integer,primary_key=True,index=True)
+  full_name=Column(String,nullable=False)
+  email=Column(String,nullable=False)
+  password = Column(String, nullable=False)
+
+  phone = Column(String, nullable=False)
+
+  role = Column(SqlEnum(UserRole), nullable=False)
